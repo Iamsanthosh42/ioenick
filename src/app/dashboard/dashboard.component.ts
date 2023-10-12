13db585dfea368e9205095f0bc79ad4c93f064d0
Iamsanthosh42 <141031966+Iamsanthosh42@users.noreply.github.com>
2023-10-12 12:18:10 +0530

    Add files via upload

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+// currentvalue: string = '';
+// currentTime:string = '';
+
+// private updateGreeting(){
+//   const currentTime = new Date() ;
+//   const hours = currentTime.getHours();
+//   const minutes = currentTime.getMinutes();
+//   let greeting = '';
+
+//   if(hours >=5 && hours < 12){
+//     greeting = 'Good Morning';
+//   }
+//   else  if(hours >=12 && hours < 18){
+//     greeting = 'Good Afternoon'
+//   }
+//   else{
+//     greeting = 'Good Evening'
+//   }
+//   this.currentvalue = `${greeting}`
+// }
+}
